4b8ca7864a168ae5f1af8a3a2288efc438d3b73e
Jason Flores Torres <jasse.ft2412@gmail.com>
2024-02-04 00:13:09 -0600

    Se agrega la llamada al Menu desde la clase principal Biblioteca, cambia el diseño

--- src/biblioteca/Biblioteca.java
+++ src/biblioteca/Biblioteca.java
@@ -14,7 +14,8 @@ public class Biblioteca {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Menu menuForm = new Menu();
+        menuForm.setVisible(true);
     }
     
 }
